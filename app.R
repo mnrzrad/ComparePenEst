6653829839bfae5e71de805cfdb625222aa24733
mnrzrad <mina.norouzirad@gmail.com>
2024-08-22 14:39:54 +0100

    update

--- app.R
+++ app.R
@@ -1,4 +1,5 @@
 library(shiny)
+library(shinyjs)
 library(bslib)
 library(glmnet)
 library(latex2exp)
@@ -9,12 +10,27 @@ source('helper.R')
 ui <- page_sidebar(
   title = "Penalized estimators",
   sidebar = sidebar(
+    # numericInput(
+    #   "sd_x1",
+    #   "Standard deviation of X1:",
+    #   value = 1, 
+    #   min = 0.01,
+    #   step = 0.1
+    # ),
+    # numericInput(
+    #   "sd_x2",
+    #   "Standard deviation of X2:",
+    #   value = 1, 
+    #   min = 0.01,
+    #   step = 0.1
+    # ),
     radioButtons(
       "Estimators",
       "Select one of the estimators",
       choices = list(
         "Ridge",
         "LASSO",
+        # "ElasticNet",
         "adaptiveLASSO",
         "marginalizedLASSO"
       ),
@@ -25,18 +41,63 @@ ui <- page_sidebar(
       "Lambda Index:",
       min = 0,
       max = 20,
-      value = 2,
+      value = 0,
       step = 2,
       animate = TRUE # Add animation to the slider
     )
   ),
-  card(
-    card_header(uiOutput("card_header")),
-    plotOutput("plot")
+  navset_card_tab(
+    id = "tabs",
+    title = uiOutput("card_header"),
+    
+    nav_panel(
+      title = "Explanations",
+      card_body(
+        uiOutput("explanation")
+      )
+    ),
+    
+    # nav_panel(
+    #   title = "Histograms",
+    #   card_body_fill(
+    #     fluidRow(
+    #       column(6, plotOutput("hist_x1")),
+    #       column(6, plotOutput("hist_x2"))
+    #     )
+    #   )
+    # ),
+    
+    nav_panel(
+      title = "Plot",
+      card_body_fill(
+        plotOutput("plot")
+      )
+    )
   )
 )
 
-server <- function(input, output) {
+server <- function(input, output, session) {
+  # data_reactive <- reactiveVal(NULL)
+  # 
+  # observe( 
+  # {
+  #   sd_x1 <- input$sd_x1
+  #   sd_x2 <- input$sd_x2
+  #   
+  #   new_data <- generate_data(sd_x1, sd_x2)
+  #   
+  #   data_reactive(new_data)
+  #   
+  #   cat("Updated data with sd_x1:", sd_x1, " sd_x2:", sd_x2, "\n")
+  # }
+  # )
+  
+  # observe({
+  #   if (is.null(data_reactive())) {
+  #     data_reactive(generate_data(input$sd_x1, input$sd_x2))
+  #   }
+  # })
+  
   output$card_header <- renderUI(
     {
     estimator <- input$Estimators
@@ -45,75 +106,47 @@ server <- function(input, output) {
     }
   )
   
+  output$explanation <- renderUI({
+    explanation <- getExplanation(input$Estimators)
+    tagList(
+      div(
+        h4(explanation$author, style = "text-align: center;"),
+        if (exists("photos", where = explanation)) {
+          # Display multiple photos for Elastic Net
+          fluidRow(
+            lapply(explanation$photos, function(photo) {
+              column(6, img(src = photo, style = "display: block; margin-left: auto; margin-right: auto; max-height: 100px; width: auto;"))
+            })
+          )
+        } else {
+          # Display a single photo for other estimators
+          img(src = explanation$photo, style = "display: block; margin-left: auto; margin-right: auto; max-height: 100px; width: auto;")
+        },
+        style = "text-align: center;"
+      ),
+      div(
+        withMathJax(paste0(explanation$formula)),
+        style = "text-align: center; margin-top: 10px;"
+      ),
+      p("Reference:", explanation$reference)
+    )
+  })
+  
+  # output$hist_x1 <- renderPlot({
+  #   req(data_reactive())
+  #   data <- data_reactive()
+  #   hist(data$x1, main = "Histogram of x1", xlab = "x1", col = "lightblue", border = "black")
+  # })
+  # 
+  # output$hist_x2 <- renderPlot({
+  #   req(data_reactive())
+  #   data <- data_reactive()
+  #   hist(data$x2, main = "Histogram of x2", xlab = "x2", col = "lightgreen", border = "black")
+  # })
   
   output$plot <- renderPlot({
     plotFunction(value = input$lambda, type = input$Estimators)
   })
 }
   
-
-
-  
-
- 
-  # output$ridgePlot <- renderPlot({
-  #   
-  #   k1 <- seq(1, 5, by = 0.01)
-  #   
-  #   k1 <- k1[k1 > min(rss_lstsq)]
-  #   
-  #   r1 <- find_closest(apply(ridge$beta, 2, function(x) sum((z0 - x[1] * z1 - x[2] * z2) ^ 2)), k1[lambdaIndex])
-  #   
-  #   plot(1.02 * range(s), 1.02 * range(s), type = "n", axes = FALSE, xlab = " ", ylab = " ", main = "(a) Ridge Regression")
-  #   axis(side = 1, pos = 0, col = "black", at = seq(-1, 1, length = 5))
-  #   axis(side = 2, pos = 0, col = "black", at = seq(-1, 1, length = 5))
-  #   text(0.05, 1, TeX("$\\beta_2$"))
-  #   text(0.98, 0.08, TeX("$\\beta_1$"))
-  #   
-  #   contour(s, s, matrix(rss_lstsq, nrow = n_lstsq), levels = k1, add = TRUE, col = "gray", drawlabels = FALSE)
-  #   contour(s, s, matrix(rss_lstsq, nrow = n_lstsq), levels = k1[1], add = TRUE, col = "black", drawlabels = FALSE)
-  #   draw_circle <- function(r, lwd = 1, col = "red") {
-  #     radians <- seq(0, 2 * pi, length = 100)
-  #     lines(r * sin(radians), r * cos(radians), col = col, lwd = lwd)
-  #   }
-  #   draw_circle(sqrt(lstsq_beta[1]^2 + lstsq_beta[2]^2))
-  #   points(lstsq_beta[1], lstsq_beta[2], pch = 19, col = "red")
-  #   
-  #   draw_circle(sqrt(ridge$beta[1, r1]^2 + ridge$beta[2, r1]^2), col = "blue")
-  #   arrows(lstsq_beta[1], lstsq_beta[2], ridge$beta[1, r1], ridge$beta[2, r1], len = 0.05, col = "blue")
-  # })
-  # 
-  # output$lassoPlot <- renderPlot({
-  #   lambdaIndex <- input$lambdaIndex
-  #   k1 <- seq(1, 5, by = 0.01)
-  #   k1 <- c(0.1 * k1, k1, 10 * k1, 100 * k1, 1000 * k1)
-  #   k1 <- k1[k1 > min(rss_lstsq)]
-  #   
-  #   r1 <- find_closest(apply(lasso$beta, 2, function(x) sum((z0 - x[1] * z1 - x[2] * z2) ^ 2)), k1[lambdaIndex])
-  #   
-  #   plot(1.02 * range(s), 1.02 * range(s), type = "n", axes = FALSE, xlab = " ", ylab = " ", main = "(b) LASSO Regression")
-  #   axis(side = 1, pos = 0, col = "black", at = seq(-1, 1, length = 5))
-  #   axis(side = 2, pos = 0, col = "black", at = seq(-1, 1, length = 5))
-  #   text(0.05, 1, TeX("$\\beta_2$"))
-  #   text(0.98, 0.08, TeX("$\\beta_1$"))
-  #   
-  #   contour(s, s, matrix(rss_lstsq, nrow = n_lstsq), levels = k1, add = TRUE, col = "gray", drawlabels = FALSE)
-  #   contour(s, s, matrix(rss_lstsq, nrow = n_lstsq), levels = k1[1], add = TRUE, col = "black", drawlabels = FALSE)
-  #   
-  #   d0 <- abs(lstsq_beta[1]) + abs(lstsq_beta[2])
-  #   segments(d0, 0, 0, d0, col = "red")
-  #   segments(0, d0, -d0, 0, col = "red")
-  #   segments(-d0, 0, 0, -d0, col = "red")
-  #   segments(0, -d0, d0, 0, col = "red")
-  #   points(lstsq_beta[1], lstsq_beta[2], pch = 19, col = "red")
-  #   
-  #   d1 <- abs(lasso$beta[1, r1]) + abs(lasso$beta[2, r1])
-  #   segments(d1, 0, 0, d1, col = "blue")
-  #   segments(0, d1, -d1, 0, col = "blue")
-  #   segments(-d1, 0, 0, -d1, col = "blue")
-  #   segments(0, -d1, d1, 0, col = "blue")
-  #   arrows(lstsq_beta[1], lstsq_beta[2], lasso$beta[1, r1], lasso$beta[2, r1], len = 0.05, col = "blue")
-  # })
-# }
-
 shinyApp(ui = ui, server = server)
--- helper.R
+++ helper.R
@@ -1,17 +1,19 @@
 library('glmnet')
 
-set.seed(888)
+
 # x1 <- rnorm(50, sd = 1)
 # x2 <- rnorm(50,mean = 0.1,sd=2)
-x1 <- rnorm(50, sd = 1)
-x2 <- x1 + rnorm(50,mean = 0.1,sd=1.5)
+
+x1 <- rnorm(50, sd = 0.5)
+x2 <- x1 + rnorm(50, mean = 0.5, sd = 2)
+  
 beta1 = 0.6
 beta2 = 0.4
 epsilon <- rnorm(50)
 y = x1*beta1 + x2*beta2 + epsilon
 
 standardize <- function(x) {
-  (x-mean(x))/sd(x)
+  (x-mean(x))#/sd(x)
 }
 
 z0 <- standardize(y)
@@ -19,7 +21,6 @@ z1 <- standardize(x1)
 z2 <- standardize(x2)
 lstsq <- lm(z0~z1+z2-1)
 lstsq_beta <- coef(lstsq)
-
 
 n_lstsq <- 50
 s <- seq(-1, 1, length=n_lstsq)
@@ -32,7 +33,7 @@ for (i in 1:n_lstsq) {
 
 s2 = sum(lstsq$residuals**2)/(n_lstsq)
 Z <- cbind(z1,z2) #cbind(x1,x2)
-mg <- s2*diag(solve(t(Z)%*%Z))
+mg <- s2 / diag(solve(t(Z)%*%Z))
 
 # persp(s, s, rss_lstsq, xlab="beta1", ylab="beta2", zlab="rss_lstsq")
 
@@ -59,12 +60,30 @@ draw_circle <- function(r,lwd = 1, col = "red") {
   lines(r*sin(radians), r*cos(radians),col = col ,lwd = lwd)
 }
 
-draw_diamond<- function(d, lwd, col){
+draw_diamond<- function(d, w, lwd, col){
   segments( d, 0, 0, d,col ,lwd, lty = 1)
   segments( 0, d,-d, 0,col ,lwd, lty = 1)
   segments(-d, 0, 0,-d,col ,lwd, lty = 1)
   segments( 0,-d, d, 0,col ,lwd, lty = 1)
 }
+
+draw_combined_penalty <- function(r, d, alpha, lwd = 1) {
+  # Define the number of points to draw
+  n <- 100
+  t <- seq(0, 2*pi, length.out = n)
+  
+  # Draw circle
+  circle_x <- r * cos(t)
+  circle_y <- r * sin(t)
+  lines(circle_x, circle_y, col = "red", lwd = lwd)
+  
+  # Draw diamond
+  diamond_x <- c(d, 0, -d, 0, d)
+  diamond_y <- c(0, d, 0, -d, 0)
+  lines(diamond_x, diamond_y, col = "blue", lwd = lwd)
+}
+
+
 
 ridge <- glmnet(cbind(z1, z2), z0, 
                 alpha=0, intercept=FALSE, nlambda=1000)
@@ -98,14 +117,21 @@ for (i in 1:m_mglasso) {
   rss_mglasso[i] <- sum((z0 - mglasso$beta[1, i]*z1 -mglasso$beta[2, i]*z2)^2)
 }
 
+enet <- glmnet(x = cbind(z1, z2), y = z0, alpha = 0.5,intercept=FALSE, nlambda=1000, penalty.factor = 1 / mg)
+m_enet <- dim(enet$beta)[2]
+rss_enet <- rep(NA,m_enet)
+for (i in 1:m_enet) {
+  rss_enet[i] <- sum((z0 - enet$beta[1, i]*z1 -enet$beta[2, i]*z2)^2)
+}
 
-k1 <- c(0, 1, 1.1, 1.2, 1.5, 2, 2.5, 3:9)
-k1 <- c(0.1 * k1, k1, 10 * k1, 100 * k1, 1000 * k1)
 
-# 
-plotFunction <- function(value, type) {
-  # Define contour levels with k1
-  k1 <- min(rss_lstsq) + c(0.1, 0.5, seq(1, 20, 2), seq(30, 1000, 10))
+# k1 <- c(0, 1, 1.1, 1.2, 1.5, 2, 2.5, 3:9)
+# k1 <- c(0.1 * k1, k1, 10 * k1, 100 * k1, 1000 * k1)
+# Define contour levels with k1
+k1 <- min(rss_lstsq) + c(0.1, 0.5, seq(1, 20, 2), seq(30, 1000, 10))
+ 
+plotFunction <- function(value, type, data) {
+  
   k1_filtered <- k1[k1 <= (min(rss_lstsq) + value)]
   
   # Draw axes
@@ -161,11 +187,11 @@ plotFunction <- function(value, type) {
       if(i == 1){
         points(lstsq_beta[1], lstsq_beta[2], pch = 19, col = "red", cex = 1)
         d0 <-  abs(lstsq_beta[1])+abs(lstsq_beta[2])
-        draw_diamond(d0, lwd = 3, col = "blue")
+        draw_diamond(d0, 1, lwd = 3, col = "blue")
       }else{
         r <- find_closest(rss_lasso, k1_filtered[i])
         d <- abs(lasso$beta[1, r])+abs(lasso$beta[2, r])
-        draw_diamond(d, lwd = 3, col = "blue")
+        draw_diamond(d, 1, lwd = 3, col = "blue")
         if(i==2){
           arrows(lstsq_beta[1], lstsq_beta[2], lasso$beta[1, r], lasso$beta[2, r], len=0.05,lwd = 4, col = 'green')
         }else{
@@ -177,16 +203,41 @@ plotFunction <- function(value, type) {
     }
   }
   
+  # if(type == "ElasticNet"){
+  #   for (i in seq_along(k1_filtered)) {
+  #     if(i == 1){
+  #       points(lstsq_beta[1], lstsq_beta[2], pch = 19, col = "red", cex = 1)
+  #       d0 <-  abs(lstsq_beta[1])+abs(lstsq_beta[2])
+  #       draw_combined_penalty(d0, d0, alpha= 0.5, lwd = 2)
+  #     }else{
+  #       r <- find_closest(rss_enet, k1_filtered[i])
+  #       d <- abs(lasso$beta[1, r])+abs(lasso$beta[2, r])
+  #       R <- sqrt(ridge$beta[1, r]^2 + ridge$beta[2, r]^2)
+  #       draw_combined_penalty(R, d, alpha= 0.5, lwd = 2)
+  #       if(i==2){
+  #         arrows(lstsq_beta[1], lstsq_beta[2], enet$beta[1, r], enet$beta[2, r], len=0.05,lwd = 4, col = 'green')
+  #       }else{
+  #         r0 <- find_closest(rss_enet, k1_filtered[i-1])
+  #         arrows(enet$beta[1, r0], enet$beta[2, r0], enet$beta[1, r], enet$beta[2, r], len=0.05, lwd = 4, col = 'green')
+  #         
+  #       }
+  #     }
+  #   }
+  # }
+  
+  
   if(type == "adaptiveLASSO"){
     for (i in seq_along(k1_filtered)) {
       if(i == 1){
         points(lstsq_beta[1], lstsq_beta[2], pch = 19, col = "red", cex = 1)
         d0 <-  abs(lstsq_beta[1])+abs(lstsq_beta[2])
-        draw_diamond(d0, lwd = 3, col = "blue")
+        w <- abs(lstsq_beta[1])/abs(lstsq_beta[2])
+        draw_diamond(d0, w, lwd = 3, col = "blue")
       }else{
         r <- find_closest(rss_alasso, k1_filtered[i])
         d <- abs(alasso$beta[1, r])+abs(alasso$beta[2, r])
-        draw_diamond(d, lwd = 3, col = "blue")
+        w <- abs(lstsq_beta[1])/abs(lstsq_beta[2])
+        draw_diamond(d, w, lwd = 3, col = "blue")
         if(i==2){
           arrows(lstsq_beta[1], lstsq_beta[2], alasso$beta[1, r], alasso$beta[2, r], len=0.05,lwd = 4, col = 'green')
         }else{
@@ -202,11 +253,13 @@ plotFunction <- function(value, type) {
       if(i == 1){
         points(lstsq_beta[1], lstsq_beta[2], pch = 19, col = "red", cex = 1)
         d0 <-  abs(lstsq_beta[1])+abs(lstsq_beta[2])
-        draw_diamond(d0, lwd = 3, col = "blue")
+        w <- mg[1]/mg[2]
+        draw_diamond(d0, w, lwd = 3, col = "blue")
       }else{
         r <- find_closest(rss_mglasso, k1_filtered[i])
         d <- abs(mglasso$beta[1, r])+abs(mglasso$beta[2, r])
-        draw_diamond(d, lwd = 3, col = "blue")
+        w <- mg[1]/mg[2]
+        draw_diamond(d, w,  lwd = 3, col = "blue")
         if(i==2){
           arrows(lstsq_beta[1], lstsq_beta[2], mglasso$beta[1, r], mglasso$beta[2, r], len=0.05,lwd = 4, col = 'green')
         }else{
@@ -215,5 +268,58 @@ plotFunction <- function(value, type) {
         }
       }
     }
+  }
+}
+
+
+
+
+getExplanation <- function(estimator) {
+  if (estimator == "Ridge") {
+    list(
+      author = "Hoerl, Arthur E., and Kennard, Robert W.",
+      photos = c(
+        "https://media.licdn.com/dms/image/v2/C4E22AQGlRxiu0U_d_Q/feedshare-shrink_800/feedshare-shrink_800/0/1604091944335?e=1727308800&v=beta&t=ccm43ntOgBF5xmzB1LBsS1mO1xspVRCraS7O-inIj0M",
+        "https://k2-storage-prod.s3.amazonaws.com/storage/userimg/original/20240520045812-6062379b39cf44248fa8548ba7c6a9e5-original.jpg"
+      ),
+      formula = "$$\\hat{\\beta}^{\\textrm{ridge}} = \\arg\\min_{\\beta} \\left\\{ \\|y - X\\beta\\|_2^2 + \\lambda \\|\\beta\\|_2^2\\right\\}$$",
+      reference = "Hoerl, A. E., & Kennard, R. W. (1970). Ridge Regression: Biased Estimation for Nonorthogonal Problems. Technometrics, 12(1), 55–67. https://doi.org/10.2307/1267351"
+    )
+  } else if (estimator == "LASSO") {
+    list(
+      author = "Tibshirani, Robert",
+      photo = "https://statistics.stanford.edu/sites/statistics/files/styles/hs_medium_square_360x360/public/media/people/tibshirani_new.jpg?h=dc1a5897&itok=G95tWrMG",
+      formula = "$$\\hat{\\beta}^{\\textrm{lasso}} = \\arg\\min_{\\beta} \\left\\{\\|y - X\\beta\\|_2^2 + \\lambda \\|\\beta\\|_1\\right\\}$$",
+      reference = "Tibshirani, R. (1996). Regression Shrinkage and Selection via the Lasso. Journal of the Royal Statistical Society. Series B (Methodological), 58(1), 267–288. http://www.jstor.org/stable/2346178"
+    )
+  } else if (estimator == "adaptiveLASSO") {
+    list(
+      author = "Zou, Hui",
+      photo = "http://users.stat.umn.edu/~zouxx019/myphotos/zou-department-photo.jpg",
+      formula = "$$\\hat{\\beta}^{\\textrm{adaptiveLASSO}} = \\arg\\min_{\\beta} \\left\\{\\|y - X\\beta\\|_2^2 + \\lambda \\sum_{j} w_j |\\beta_j|\\right\\} \\quad \\text{where } w_j = (|\\beta^{\\textrm{OLS}}_j|)^{-1}$$",
+      reference = "Zou, H. (2006). The Adaptive Lasso and Its Oracle Properties. Journal of the American Statistical Association, 101(476), 1418–1429. http://www.jstor.org/stable/27639762"
+    )
+  } else if (estimator == "marginalizedLASSO") {
+    list(
+      author = "A. K. Md. Ehsanes Saleh",
+      photo = "https://carleton.ca/math/wp-content/uploads/a_k_saleh-240x240.jpg",  # Replace with an actual URL
+      formula = "$$
+                \\begin{align*}
+                \\hat{\\beta}^{\\textrm{marginalizedLASSO}} &= \\arg\\min_{\\beta} \\left\\{\\|y - X\\beta\\|_2^2 + \\lambda \\sum_{j} C^{jj} |\\beta_j|\\right\\} \\
+                & \\text{where } C^{jj} \\text{ denotes the } j\\text{th diagonal of } \\sigma^2(X^\\top X)^{-1}
+                \\end{align*}
+                $$",  # Update with the correct formula
+      reference = "Saleh, A. K. Md. E. S., Arashi, M., Saleh, R., (2022). Rank-Based Methods for Shrinkage and Selection: With Application to Machine Learning, Wiley"
+    )
+  }else if (estimator == "ElasticNet") {
+    list(
+      author = "Zou, Hui and Hastie, Trevor",
+      photos = c(
+        "http://users.stat.umn.edu/~zouxx019/myphotos/zou-department-photo.jpg",
+        "https://statistics.stanford.edu/sites/statistics/files/styles/hs_medium_square_360x360/public/media/people/hastie_new.jpg?h=07d0026a&itok=6KSgB3EY"
+      ),
+      formula = "$$\\hat{\\beta}^{\\textrm{elastic net}} = \\arg\\min_{\\beta} \\left\\{\\|y - X\\beta\\|^2 + \\lambda \\left( \\alpha \\|\\beta\\|_1 + (1 - \\alpha) \\|\\beta\\|_2^2\\right)\\right\\}$$",
+      reference = "Zou, H., & Hastie, T. (2005). Regularization and Variable Selection via the Elastic Net. Journal of the Royal Statistical Society. Series B (Statistical Methodology), 67(2), 301–320. http://www.jstor.org/stable/3647580"
+    )
   }
 }
